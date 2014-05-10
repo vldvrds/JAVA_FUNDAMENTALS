767c0b126e2d1355123cd71d9a83400f8f6ce78b
vldvrds <vldvrds@gmail.com>
2014-05-10 10:47:55 +0100

    Hello word project with read 1

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -14,16 +14,17 @@ public class Main {
 		String sampleString = br.readLine();
 		
 		String[] tempStrings=sampleString.split("[ ]");
-		
-		for (String s:tempStrings){
-			if (s.equals(tempStrings[tempStrings.length-1])){
-				System.out.println(s);
-			}else{
-				System.out.print(s+" ");
+		if(tempStrings!=null && tempStrings.length>0){
+			for (String s:tempStrings){
+				if (tempStrings[tempStrings.length-1].equals(s)){
+					System.out.println(s);
+				}else{
+					System.out.print(s+" ");
+				}
 			}
-		}
-		for (String string : tempStrings) {
-			System.out.println(string);
+			for (String string : tempStrings) {
+				System.out.println(string);
+			}
 		}
 		if(args!=null && args.length>0){
 			for(String s:args){
@@ -31,6 +32,37 @@ public class Main {
 			}
 		}else{
 			System.out.println("Sorry, args is empty!");
+		}
+		
+		int j=0;
+		if(args!=null && args.length>0){
+			for(;j>args.length;j++){
+				System.out.print(args[j]+" ");
+			}
+			System.out.println(args[j]);
+			
+		}
+		int g=0;
+		if(tempStrings!=null && tempStrings.length>0){
+			while(g<tempStrings.length-1){
+				System.out.print(tempStrings[g]+" ");
+				g++;
+			}
+			System.out.println(tempStrings[g]);
+		}
+		int h=0;
+		if(tempStrings!=null && tempStrings.length>0){
+			do{
+				if(tempStrings.length==h)
+					System.out.print(tempStrings[h]+"/n");
+				else
+					System.out.print(tempStrings[h]+" ");
+				h++;
+			}while(h<tempStrings.length);
+		}
+		System.out.println();
+		for(int x=0; x<tempStrings.length;x++){
+			System.out.print(tempStrings[x]+((x==tempStrings.length-1)? "":" "));
 		}
 	
 	}
